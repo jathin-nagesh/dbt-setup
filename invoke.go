c4a7038879c87c169a21eeb1b2be880f55fc5c2c
jathin-nagesh <jathin.nagesh@ingka.ikea.com>
2022-09-27 16:17:33 +0200

    Removing the local cred json file

--- invoke.go
+++ invoke.go
@@ -7,7 +7,7 @@ import (
         "os/exec"
 )
 func handler(w http.ResponseWriter, r *http.Request) {
-        log.Print("helloworld: received a request")
+        log.Print("dbt-setup: received a request")
 cmd := exec.Command("/bin/sh", "script.sh")
         cmd.Stdout = os.Stdout
  cmd.Stderr = os.Stderr
@@ -17,12 +17,12 @@ cmd := exec.Command("/bin/sh", "script.sh")
  }
 }
 func main() {
-        log.Print("helloworld: starting server...")
+        log.Print("dbt-setup: starting server...")
 http.HandleFunc("/", handler)
 port := os.Getenv("PORT")
         if port == "" {
                 port = "8080"
         }
-log.Printf("helloworld: listening on %s", port)
+log.Printf("dbt-setup: listening on %s", port)
         log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
